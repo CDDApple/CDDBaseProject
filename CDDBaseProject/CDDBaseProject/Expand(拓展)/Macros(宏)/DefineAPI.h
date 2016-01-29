935ef4a33c614bda29eaa9c52ed10dca98a8241d
caodongdong <cdd19910120@sina.com>
2016-01-29 10:56:21 +0800

    公共宏文件添加

---
+++ CDDBaseProject/CDDBaseProject/Expand(拓展)/Macros(宏)/DefineAPI.h
@@ -0,0 +1,15 @@
+//
+//  DefineAPI.h
+//  CDDBaseProject
+//
+//  Created by 曹冬冬 on 16/1/29.
+//  Copyright © 2016年 CDD. All rights reserved.
+//
+
+#ifndef DefineAPI_h
+#define DefineAPI_h
+
+//定义一个API
+#define APIURL                @"http://xxxxx/"
+
+#endif /* DefineAPI_h */
---
+++ CDDBaseProject/CDDBaseProject/Expand(拓展)/Macros(宏)/DefineUI.h
@@ -0,0 +1,57 @@
+//
+//  DefineUI.h
+//  CDDBaseProject
+//
+//  Created by 曹冬冬 on 16/1/29.
+//  Copyright © 2016年 CDD. All rights reserved.
+//
+
+#ifndef DefineUI_h
+#define DefineUI_h
+
+/** 字体大小 */
+#define NAME_FONT(NAME,FONTSIZE)     [UIFont fontWithName:(NAME) size:(FONTSIZE)]
+#define S_FONT(FONTSIZE)        [UIFont systemFontOfSize:FONTSIZE]
+#define S_BOLD_FONT(FONTSIZE)   [UIFont boldSystemFontOfSize:FONTSIZE]
+
+
+//----------------------颜色类---------------------------
+// rgb颜色转换（16进制->10进制）
+#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+
+//带有RGBA的颜色设置
+#define COLOR(R, G, B, A) [UIColor colorWithRed:R/255.0 green:G/255.0 blue:B/255.0 alpha:A]
+
+// 获取RGB颜色
+#define RGBA(r,g,b,a) [UIColor colorWithRed:r/255.0f green:g/255.0f blue:b/255.0f alpha:a]
+#define RGB(r,g,b) RGBA(r,g,b,1.0f)
+
+//背景色
+#define BACKGROUND_COLOR [UIColor colorWithRed:242.0/255.0 green:236.0/255.0 blue:231.0/255.0 alpha:1.0]
+
+//清除背景色
+#define CLEARCOLOR [UIColor clearColor]
+
+#pragma mark - color functions
+#define RGBCOLOR(r,g,b) [UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:1]
+#define RGBACOLOR(r,g,b,a) [UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:(a)]
+
+//----------------------颜色类--------------------------
+
+
+/** 资源路径 */
+#define PNG_PATH(NAME) [[NSBundle mainBundle] pathForResource:[NSString stringWithUTF8String:NAME] ofType:@"png"]
+#define JPG_PATH(NAME) [[NSBundle mainBundle] pathForResource:[NSString stringWithUTF8String:NAME] ofType:@"jpg"]
+#define PATH(NAME,EXT) [[NSBundle mainBundle] pathForResource:(NAME) ofType:(EXT)]
+
+/** 加载图片 */
+#define PNG_IMAGE_FILE(NAME)        [UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:(NAME) ofType:@"png"]]
+#define JPG_IMAGE_FILE(NAME)        [UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:(NAME) ofType:@"jpg"]]
+#define IMAGE_FILE(NAME,EXT)        [UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:(NAME) ofType:(EXT)]]
+#define IMAGE_NAMED(NAME)           [UIImage imageNamed:NAME]
+
+/** UIVIew */
+//设置View的tag属性
+#define VIEWWITHTAG(_OBJECT, _TAG)    [_OBJECT viewWithTag : _TAG]
+
+#endif /* DefineUI_h */
